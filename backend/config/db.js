1ceb5f9ff7f3d5b2dae2a6fcaf5f2eec046fa6e4
pvariya <variyapurv4211@gmail.com>
2025-03-15 03:58:28 -0700

    project 99% complate

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDb =async()=>{
     try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/e-com")
+        await mongoose.connect("mongodb+srv://variyapurv4211:purv@cluster0.ykwwg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
         console.log("Connected to MongoDB");
     } catch (error) {
         console.error("Error connecting to MongoDB", error)
